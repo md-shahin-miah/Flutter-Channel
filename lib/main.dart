7a64c971ebb2844eabf184a0775576b7479f5931
shahin_yotech <shahin@yotech.ltd>
2024-10-04 11:38:28 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_channel/screens/basic_channel_demo.dart';
+import 'package:flutter_channel/screens/binary_message_demo.dart';
+import 'package:flutter_channel/screens/event_channel_demo.dart';
+import 'package:flutter_channel/screens/method_channel_demo.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key,});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title:const Text("Platform Channels"),
+      ),
+      body: Center(
+        child: ListView(
+          children:  [
+            ListTile(
+              title: const Text("Binary Message"),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) =>const BinaryMessageDemo()));
+              },
+            ),
+            ListTile(
+              title: const Text("MethodChannel"),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) =>const MethodChannelDemo()));
+              },
+            ),
+            ListTile(
+              title: const Text("EventChannel"),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) =>const EventChannelDemo()));
+              },
+            ),
+            ListTile(
+              title: const Text("BasicMessageChannel"),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) =>const BasicChannelDemo()));
+              },
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
---
+++ lib/screens/event_channel_demo.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../channels/event_channel_timer.dart';
+
+class EventChannelDemo extends StatefulWidget {
+  const EventChannelDemo({super.key});
+
+  @override
+  State<EventChannelDemo> createState() => _EventChannelDemoState();
+}
+
+class _EventChannelDemoState extends State<EventChannelDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Event Channel Demo"),
+      ),
+      body: Center(
+        child: StreamBuilder<int>(
+          stream: EventChannelTimer.timerValue,
+          builder: (context, snapshot) {
+            return switch (snapshot) {
+              AsyncSnapshot(hasError: true) =>
+                  Text((snapshot.error as PlatformException).message!),
+              AsyncSnapshot(hasData: true) => Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text('Data: ${snapshot.data!.toStringAsFixed(3)}',
+                      style: const TextStyle(fontSize: 32)),
+                ],
+              ),
+              _ =>
+              const Text('No Data Available', style: TextStyle(fontSize: 32)),
+            };
+          },
+        ),
+      ),
+    );
+  }
+}
